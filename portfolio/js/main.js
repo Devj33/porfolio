704c28fb9418327998d79a9e0679f4c2514dbc17
muskanniaj <muskan.niaj@gmail.com>
2021-07-22 13:18:43 +0530

    commit

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,6 +1,17 @@
 (function ($) {
     "use strict";
 
+// Navbar on scrolling
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+        $('.navbar').fadeIn('slow').css('display', 'flex');
+    } else {
+        $('.navbar').fadeOut('slow').css('display', 'none');
+    }
+});
+
+
+
 
     // Typed Initiate
     if ($('.typed-text-output').length == 1) {
@@ -13,6 +24,16 @@
             loop: true
         });
     }
+
+    
+    
+    
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
 
 })(jQuery);
 
