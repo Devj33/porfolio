94fdf3f5ad5f8c60c7f1bff0edb6e9190b10d8eb
muskanniaj <muskan.niaj@gmail.com>
2021-07-25 18:00:29 +0530

    commit

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -10,7 +10,21 @@ $(window).scroll(function () {
     }
 });
 
+// Smooth scrolling on the navbar links
+$(".navbar-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
 
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 45
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.navbar-nav').length) {
+            $('.navbar-nav .active').removeClass('active');
+            $(this).closest('a').addClass('active');
+        }
+    }
+});
 
 
     // Typed Initiate
@@ -24,6 +38,15 @@ $(window).scroll(function () {
             loop: true
         });
     }
+
+    // Scroll to Bottom
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
 
     
     
